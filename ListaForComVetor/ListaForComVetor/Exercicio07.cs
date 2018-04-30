d089d354226c1dab6b21b3850bd2390299d979b5
AdrianBSilva <38699788+AdrianBSilva@users.noreply.github.com>
2018-04-30 17:25:42 -0300

    Adicionado o Exercicio 7 e 8

---
+++ ListaForComVetor/ListaForComVetor/Exercicio07.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ListaForComVetor
+{
+    class Exercicio07
+    {
+        public Exercicio07()
+        {
+            char[] caracteres = new char[5];
+            bool validar = true;
+            int quantidadeVogais = 0, quantidadeConsoante = 0, quantidadeEspeciais = 0;
+            string vogais = "", consoantes = "", especiais = "";
+            
+            for(int i = 0; i < caracteres.Length; i++, validar = true )
+            {
+                
+               while (validar == true)
+                {
+                    try
+                    {
+                        Console.Write("Caracter: ");
+                        caracteres[i] = Convert.ToChar(Console.ReadLine().ToLower());
+                        validar = false;
+                        Console.Clear();
+                    }
+                    catch
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Apenas um caracter");
+
+                    }
+                }
+                
+                if ((caracteres[i] == 'a') || (caracteres[i] == 'e') || (caracteres[i] == 'i')
+                    || (caracteres[i] == 'o') || (caracteres[i] == 'u'))
+                {
+                    quantidadeVogais++;
+                    vogais = vogais + caracteres[i] + ", ";
+                }
+                if ((caracteres[i] == 'b') || (caracteres[i] == 'c') || (caracteres[i] == 'd') ||
+                    (caracteres[i] == 'f') || (caracteres[i] == 'g') || (caracteres[i] == 'h') ||
+                    (caracteres[i] == 'j') || (caracteres[i] == 'k') || (caracteres[i] == 'l') ||
+                    (caracteres[i] == 'm') || (caracteres[i] == 'n') || (caracteres[i] == 'p') ||
+                    (caracteres[i] == 'q') || (caracteres[i] == 'r') || (caracteres[i] == 's') ||
+                    (caracteres[i] == 't') || (caracteres[i] == 'v') || (caracteres[i] == 'w') ||
+                     (caracteres[i] == 'x') || (caracteres[i] == 'y') || (caracteres[i] == 'z'))
+                {
+                    quantidadeConsoante++;
+                    consoantes = consoantes + caracteres[i] + ", ";
+                }
+                if ((caracteres[i] != 'b') && (caracteres[i] != 'c') && (caracteres[i] != 'd') && 
+                    (caracteres[i] != 'f') && (caracteres[i] != 'g') && (caracteres[i] != 'h') && 
+                    (caracteres[i] != 'j') && (caracteres[i] != 'k') && (caracteres[i] != 'l') && 
+                    (caracteres[i] != 'm') && (caracteres[i] != 'n') && (caracteres[i] != 'p') && 
+                    (caracteres[i] != 'q') && (caracteres[i] != 'r') && (caracteres[i] != 's') && 
+                    (caracteres[i] != 't') && (caracteres[i] != 'v') && (caracteres[i] != 'w') &&
+                     (caracteres[i] != 'x') && (caracteres[i] != 'y') && (caracteres[i] != 'z') &&
+                    (caracteres[i] != 'a') && (caracteres[i] != 'e') && (caracteres[i] != 'i') && 
+                    (caracteres[i] != 'o') && (caracteres[i] != 'u') )
+                {
+                    quantidadeEspeciais++;
+                    especiais = especiais + caracteres[i] + ", ";
+                }
+                    
+                    
+
+            }
+            Console.WriteLine("Quantidade de Vogais: " + quantidadeVogais + "\nQue são: " + vogais + "\nQuantide de Consoantes: " + quantidadeConsoante +
+                "\nQue são: " + consoantes + "\nQuantidade de caracteres especiais: " + quantidadeEspeciais + "\nQue são: " + especiais);
+        }
+    }
+}
---
+++ ListaForComVetor/ListaForComVetor/Exercicio08.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ListaForComVetor
+{
+    class Exercicio08
+    {
+        public Exercicio08()
+        {
+            bool validarQunatidadesDeNumeros = true;
+            bool validarNumero = true;
+            int cadastros = 0;
+            while (validarQunatidadesDeNumeros == true)
+            {
+
+                try
+                {
+                    Console.Write("Quantos números deseja cadastrar: ");
+                    cadastros = Convert.ToInt32(Console.ReadLine());
+                    validarQunatidadesDeNumeros = false;
+                    Console.Clear();
+                }
+                catch
+                {
+                    Console.Clear();
+                    Console.WriteLine("''Apenas números'' ");
+                }
+            }
+            int[] numeros = new int[cadastros];
+            int par = 0, impar = 0;
+            int[] numerosPares = new int[par];
+            int[] numerosImpares = new int[impar];
+            for (int i = 0; i < cadastros; i++, validarNumero = true)
+            {
+
+                while (validarNumero == true)
+                {
+
+                    try
+                    {
+                        Console.Write("Digite um número: ");
+                        numeros[i] = Convert.ToInt32(Console.ReadLine());
+                        validarNumero = false;
+                        Console.Clear();
+                    }
+                    catch
+                    {
+                        Console.Clear();
+                        Console.WriteLine("''Apenas números'' ");
+                    }
+                }
+                if (numeros[i] % 2 == 0)
+                {
+
+                    par++;
+                    
+                }
+                else if (numeros[i] % 2 != 0)
+                {
+
+                    impar++;
+                    
+                }
+                if (numeros[i] % 2 == 0)
+                {
+                    
+                    par++;
+                    numerosPares[i] = numerosPares[i] + numeros[i];
+                }
+                else if (numeros[i] % 2 != 0)
+                {
+                    
+                    impar++;
+                    numerosImpares[i] = numerosImpares[i] + numeros[i];
+                }
+                
+                
+            }
+            Console.Write("Vetor Original: ");
+            for (int i = 0; i < cadastros; i++)
+            {
+                Console.Write(numeros[i] + ", ");
+            }
+            Console.WriteLine();
+            Console.Write("Vetor Par: ");
+            for (int i = 0; i < numerosPares.Length; i++)
+            {
+
+                Console.Write(numerosPares[i] + ", ");
+            }
+            Console.WriteLine();
+            Console.Write("Vetor Impar: ");
+            for (int i = 0; i < numerosImpares.Length; i++)
+            {
+                Console.Write(numerosImpares[i] + ", ");
+            }
+            Console.WriteLine();
+
+
+
+
+        }
+    }
+}
